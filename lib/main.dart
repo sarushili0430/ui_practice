5bc416828c3abc799dc7c90a2e626d96a3c3778f
Koyu Fuke <kouyuu6@gmail.com>
2024-12-21 17:08:53 +0900

    feat: Fixed UI for Admin Mobile

--- lib/main.dart
+++ lib/main.dart
@@ -31,40 +31,180 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+  int _selectedindex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        body: SafeArea(
+            child: Row(
+      children: [
+        Container(
+            decoration: BoxDecoration(
+                border: Border(right: BorderSide(color: Colors.black))),
+            child: NavigationRail(
+                leading: Padding(
+                    padding: EdgeInsets.only(bottom: 12.0),
+                    child: Icon(Icons.format_quote_sharp)),
+                destinations: [
+                  NavigationRailDestination(
+                      icon: Icon(Icons.people),
+                      label: Text(""),
+                      padding: EdgeInsets.symmetric(vertical: 12.0)),
+                  NavigationRailDestination(
+                      icon: Icon(Icons.face),
+                      label: Text(""),
+                      padding: EdgeInsets.symmetric(vertical: 12.0)),
+                  NavigationRailDestination(
+                      icon: Icon(Icons.bookmark),
+                      label: Text(""),
+                      padding: EdgeInsets.symmetric(vertical: 12.0)),
+                ],
+                selectedIndex: _selectedindex)),
+        Expanded(
+            child: Container(
+          color: Colors.grey[300],
+          child: Column(
+            children: [
+              Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  child: PostsHeader()),
+              Expanded(child: MockCardList())
+            ],
+          ),
+        ))
+      ],
+    )));
+  }
+}
+
+class PostsHeader extends StatefulWidget {
+  const PostsHeader({super.key});
+
+  @override
+  State<PostsHeader> createState() => _PostsHeaderState();
+}
+
+class _PostsHeaderState extends State<PostsHeader> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [PostStatus(), AllTypes()],
+    ));
+  }
+}
+
+class PostStatus extends StatelessWidget {
+  const PostStatus({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final int _posts = 20;
+    return Container(
+      child: Row(
+        children: [
+          Padding(
+              padding: EdgeInsets.only(right: 8),
+              child: CircleAvatar(
+                child: Icon(Icons.list),
+                backgroundColor: Colors.grey,
+              )),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [Text("Posts"), Text("$_posts Posts")],
+          )
+        ],
       ),
-      body: Center(
+    );
+  }
+}
+
+class AllTypes extends StatelessWidget {
+  const AllTypes({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final int _posts = 20;
+    return Container(
+      height: 40,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+              padding: EdgeInsets.only(right: 8),
+              child: CircleAvatar(
+                child: Icon(Icons.list),
+                backgroundColor: Colors.grey,
+              )),
+          Text("All Types"),
+        ],
+      ),
+    );
+  }
+}
+
+class MockCardList extends StatelessWidget {
+  const MockCardList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      shrinkWrap: true,
+      children: [
+        MockCard(),
+        MockCard(),
+        MockCard(),
+        MockCard(),
+        MockCard(),
+      ],
+    );
+  }
+}
+
+class MockCard extends StatelessWidget {
+  const MockCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ListTile(
+            leading: Icon(Icons.account_circle),
+            title: Text("Pean"),
+            subtitle: Text("2 min ago")),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(width: 56),
+            Container(
+              width: 16,
+              height: 16,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.green, width: 4),
+                borderRadius: BorderRadius.circular(8),
+              ),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              "Weak reason. No action required.",
+              style: TextStyle(fontSize: 12),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        Padding(
+            padding: EdgeInsets.symmetric(vertical: 12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  "Report Details",
+                ),
+                Text("Archive"),
+                Text("Publish"),
+              ],
+            )),
+      ],
+    ));
   }
 }
